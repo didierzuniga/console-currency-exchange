78b005bb9209fef7ae6b9d61bca720fb3baa8e0b
Didier Zuñiga <didier35@hotmail.com>
2018-02-25 20:30:58 -0500

    [update] Adding currency exchange MXN to JPY

--- currency-exchange.py
+++ currency-exchange.py
@@ -7,19 +7,32 @@ def run():
 	print("::::::::::::::::::::::::::::::::::::::")
 	print(":: C U R R E N C Y  E X C H A N G E ::")
 	print("::::::::::::::::::::::::::::::::::::::\n")
-	print("¿Que deseas cambiar?\n1) MXN a USD\n2) USD a MXN")
-	valor = int(raw_input("->"))
-	ammount = int(raw_input("Cantidad a cambiar-> "))
-	exchange(valor, ammount)
+	print("""¿Que deseas cambiar?
+	1) MXN a USD
+	2) USD a MXN
+	3) MXN a JPY
+	4) JPY a MXN""")
+	valor = raw_input("->")
+	if valor != "1" and valor != "2" and valor != "3" and valor != "4":
+		print("¡Caracter no permitido!")
+	else:
+		ammount = int(raw_input("Cantidad a cambiar-> "))
+		exchange(valor, ammount)
 
 def exchange(valor, ammount):
-	#response = urlopen("https://api.fixer.io/latest")
-	if valor == 1:
+	if int(valor) == 1:
 		b = "MXN"
 		f = "USD"
-	elif valor == 2:
+	elif int(valor) == 2:
 		b = "USD"
 		f = "MXN"
+	elif int(valor) == 3:
+		b = "MXN"
+		f = "JPY"
+	elif int(valor) == 4:
+		b = "JPY"
+		f = "MXN"
+
 	path = "https://api.fixer.io/latest?base=%s" %(b)
 	response = urllib2.urlopen(path)
 	data = response.read()
@@ -28,7 +41,7 @@ def exchange(valor, ammount):
 	print(" Tarifa al día: {}".format(drt["date"]))
 	result = ammount * drt["rates"][f]
 	print(" Su cambio es ${} {}".format(result, f))
-	print("_____________________________________")
+	print("_____________________________________\n")
 	
 
 if __name__ == '__main__':
